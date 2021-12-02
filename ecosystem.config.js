684f7eacb461ce26499d44b69a9b68a74e818d4a
Brett Plemons <Brett@RevLogical.com>
2021-12-02 12:16:07 -0600

    Add eslint and prettier config, as well as pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "./src/app.js",
+      instances: "max",
+      watch: "true",
+      log_date_format: "YY-MM-DD HH:mm Z",
+      listen_timeout: "8000",
+      kill_timeout: "1600",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "https://github.com/PlemonsBrett/Portfolio",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy": "yarn && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
